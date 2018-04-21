a5c0a776e5e81b1ad09ad40de0ceda61977c3663
Yvonne Reichenbach <yvonnereichenbach@lawn-143-215-115-175.lawn.gatech.edu>
2018-04-21 09:59:51 -0400

    Parent Model Update

--- start2finish/models/parent.js
+++ start2finish/models/parent.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+const parentSchema = new Schema({
+  id: { type: String, required: true },
+  login: { type: String, required: true },
+  children [
+     {
+     	userName: {type: String, required: true},
+     	parentReward: {type: String},
+     	goals: [
+     	   {
+     	   	goalName: {type: String},
+     	   	days: {type: Number},
+     	   	pointValue: {type: Number},
+     	   }]
+     }]
 });
 
 const Parent = mongoose.model("Parent", parentSchema);
