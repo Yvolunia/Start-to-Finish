6298b249080bed01678e03638139043f5e560a52
Judi Perez <j_vperez29@bellsouth.net>
2018-04-28 14:00:14 -0400

    updated 4-28

--- start2finish/client/src/App.js
+++ start2finish/client/src/App.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Parent from "./pages/Parent";
-import Login from "./pages/Login";
-import Child from "./pages/Child";
-import Sign
-import Nav from "./components/Nav";
+import TodoItems from './pages/Parent Mission Set Up/parent';
+// import Parent from "./pages/Parent";
+// import Login from "./pages/Login";
+// import Child from "./pages/Child";
+// import Sign
+// import Nav from "./components/Nav";
+
+// const App = () => (
+//   <Router>
+//     <div>
+//       <Nav />
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/signup" component={Signup} />
+//         <Route exact path="/parent" component={Parent} />
+//         <Route exact path="/tracker" component={Tracker} />
+//         <Route exact path="/child" component={Child} />
+//         <Route exact path="/profile" component={Profile} />      
+//       </Switch>
+//     </div>
+//   </Router>
+// );
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/parent" component={Parent} />
-        <Route exact path="/tracker" component={Tracker} />
-        <Route exact path="/child" component={Child} />
-        <Route exact path="/profile" component={Profile} />      
-      </Switch>
+        <Route exact path="/todo" component={TodoItems}/>
     </div>
   </Router>
 );
--- start2finish/client/src/pages/Parent Mission Set Up/parent.js
+++ start2finish/client/src/pages/Parent Mission Set Up/parent.js
@@ -1,1 +1,134 @@
-parent.js
+import axios from 'axios';
+import React, { Component } from 'react';
+
+export default class TodoItems extends Component {
+  state = {
+    childName: "",
+    days: "",
+    missionName: "",
+    parentReward: "",
+    points: ""
+  }
+
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    axios.post('/api/parent', this.state);
+  }
+
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+        <input name="childName" value={this.state.childName} onChange={this.handleChange} placeholder="child name" />
+        <input name="days" value={this.state.days} onChange={this.handleChange} placeholder="days" />
+        <input name="missionName" value={this.state.name} onChange={this.handleChange} placeholder="mission name" />
+        <input name="parentReward" value={this.state.parentRewards} onChange={this.handleChange} placeholder="rewards" />
+        <input name="points" value={this.state.points} onChange={this.handleChange} placeholder="points" />
+        <button>Submit</button>
+      </form>
+      </div>
+    );
+  }
+}
+
+// class TodoItems {
+//   state = {
+//     childName: "",
+//     parentReward: "",
+//     missions: [] 
+//   };
+
+//   createTasks(item) {
+//     return <li key={item._id}>{item.text}</li>
+//   }
+
+//   render() {
+//     return (
+// <div className="Missions">
+//                 <div className="task1">
+//                   <form onSubmit={this.addItem}>
+//                     <input ref={(a) => this._inputElement = a}
+//                            placeholder="enter task">
+//                     </input>
+//                     <button
+
+//                 </div>
+//                 <TodoItems entries={this.state.items}/>
+//               </div>
+//     );
+//   }
+// }
+/*
+var TodoItems = React.createClass({
+  render: function() {
+     var todoEntries = this.props.entries;
+ 
+    function createTasks(item) {
+      return <li key={item.key}>{item.text}</li>
+    }
+ 
+    var listItems = todoEntries.map(createTasks);
+ 
+
+           return (
+              
+            );
+          }
+            }
+          });
+
+
+
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {
+      items: []
+    };
+  },
+ addItem: function(e) {
+  var itemArray = this.state.items;
+ 
+  itemArray.push(
+    {
+      text: this._inputElement.value,
+      key: Date.now()
+    }
+  );
+ 
+  this.setState({
+    items: itemArray
+  });
+ 
+  this._inputElement.value = "";
+ 
+  e.preventDefault();
+}
+ 
+  render: function() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a}
+                   placeholder="enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+ return (
+      <ul className="theList">
+        {listItems}
+      </ul>
+    );
+    */
