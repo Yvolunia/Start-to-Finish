849675c36d139b69eec215b6cacde8b9c553698c
Yvonne Reichenbach <yvonnereichenbach@lawn-143-215-126-128.lawn.gatech.edu>
2018-04-28 13:04:01 -0400

    Update Axios

--- start2finish/client/src/utils/API.js
+++ start2finish/client/src/utils/API.js
@@ -1,1 +1,19 @@
 import axios from "axios";
+
+export default {
+    getparents: function() {
+      return axios.get("/api/parents");
+    },
+    // Gets the book with the given id
+    getparents: function(id) {
+      return axios.get("/api/parents/" + id);
+    },
+
+    deleteparents: function(id) {
+      return axios.delete("/api/parents/" + id);
+    },
+    
+    saveparents: function(parentData) {
+      return axios.post("/api/books", parentData);
+    }
+  };
